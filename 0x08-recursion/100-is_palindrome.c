c27dae5270ae391e0a0680753ba761dcb3bd36bd
David Kasule <davidkasule9@gmail.com>
2023-08-30 15:05:00 +0000

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_palindrome(s, 0, _strlen-recursion(s)));
+	return (check_palindrome(s, 0, _strlen_recursion(s)));
 }
 
 /**
